c69519ab2304b41cbc03253124fb376c9b271ef9
Pranav Tripathi <tripathipranav14@gmail.com>
2023-10-06 11:19:55 +0530

    create footer

---
+++ components/Footer.jsx
@@ -0,0 +1,63 @@
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedinIn, FaYoutube, FaGithub } from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '@/assets/Logo.png'
+
+const Footer = () => {
+
+    const date = new Date()
+    const year = date.getFullYear()
+    return (
+        <footer className="flex flex-col bg-slate-100 text-gray-800 p-6 min-w-full">
+            <div className='w-5/6 mx-auto'>
+                <div className='flex gap-16 items-center ml-48'>
+                    <p className='text-xs font-semibold'>Keep In Touch</p>
+                    <div className='flex justify-center items-center gap-3 text-xl text-gray-700'>
+                        <Link href="#" className='transition hover:scale-105 hover:text-blue-500'><FaFacebook /></Link>
+                        <Link href="#" className='transition hover:scale-105 hover:text-purple-500'><FaInstagram /></Link>
+                        <Link href="#" className='transition hover:scale-105 hover:text-blue-400'><FaTwitter /></Link>
+                        <Link href="#" className='transition hover:scale-105 hover:text-blue-700'><FaLinkedinIn /></Link>
+                        <Link href="#" className='transition hover:scale-105 hover:text-red-500'><FaYoutube /></Link>
+                        <Link href="#" className='transition hover:scale-105 hover:text-gray-500'><FaGithub /></Link>
+                    </div>
+                    <p>Email us: <a className='text-blue-600 hover:underline' href='mailto:dsc.abesit@gmail.com'>dsc.abesit@gmail.com</a></p>
+                </div>
+                <div className='flex text-gray-700 mt-6 text-sm justify-around'>
+                    <div className=''>
+                        <div className='mb-3'><Link className='font-bold' href="#">ABOUT</Link></div>
+                        <div><Link className='' href="#">GDG Noida</Link></div>
+                        <div><Link className='font-light' href="#">Google Developers Group</Link></div>
+                        <div><Link className='font-light' href="#">Meet the Team</Link></div>
+                    </div>
+                    <div>
+                        <div className='mb-3'><Link className='font-bold' href="#">ABOUT GDSC ABESIT</Link></div>
+                        <div><Link className='font-light' href="#">Past Events</Link></div>
+                        <div><Link className='font-light' href="#">Upcoming Events</Link></div>
+                    </div>
+                    <div>
+                        <div className='mb-3'><Link className='font-bold' href="#">OTHER LINKS</Link></div>
+                        <div><Link className='font-semibold' href="#">Community Guidelines</Link></div>
+                    </div>
+                </div>
+                <hr class="h-px my-4 bg-gray-400 border-0" />
+                <div className='flex justify-between items-center'>
+                    <div className="font-semibold">
+                        <div className="flex items-center">
+                            <Image src={Logo} alt="logo" width={60} />
+                            <h2>Google Developer Student Club</h2>
+                        </div>
+                        <p className="ml-14 -mt-3 text-xs">ABES Institute of Technology</p>
+                    </div>
+                    <div>
+                        <p className='text-xs font-light'>
+                            &copy; {year} <span className='font-semibold'>Google Developer Student Club, ABESIT.</span> 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import { Inter } from 'next/font/google'
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
       <Navbar />
       <Hero />
-
+      {/* Other components */}
+      <Footer />
     </main>
   )
 }

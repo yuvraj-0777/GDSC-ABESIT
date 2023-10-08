cf7461420d7c432440d4c91df637ebb0bf28ec21
Pranav Tripathi <tripathipranav14@gmail.com>
2023-10-08 10:40:58 +0530

    Create team component

--- components/Team.jsx
+++ components/Team.jsx
@@ -11,110 +11,74 @@ const teamData = [
     {
         id: 1,
         name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'DSC Lead',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        img: 'https://res.cloudinary.com/dkgwk8ey5/image/upload/v1696613754/1689511880357_amgrmn.jpg',
+        title: 'Lead',
+        twitter: 'https://twitter.com/_keshav_malik',
+        github: 'https://github.com/keshav-0907',
     },
     {
         id: 2,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        name: 'Ishaan',
+        img: 'https://avatars.githubusercontent.com/u/113186458?v=4',
+        title: 'Open Source',
+        twitter: 'https://twitter.com/Ishaan2053',
+        github: 'https://github.com/Ishaan2053',
     },
     {
         id: 3,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        name: 'Chirag Ramesh Chandra',
+        img: 'https://media.licdn.com/dms/image/D4D03AQF3Rrlg-Uls1w/profile-displayphoto-shrink_400_400/0/1692046813033?e=1701907200&v=beta&t=Ad_JDZC_bVD4Th-MMQrcnVswbasn5vrsOiXobar6yd4',
+        title: 'App Development',
+        twitter: 'https://twitter.com/chiragchandra25',
+        github: 'https://github.com/Chiragramesh25',
     },
     {
-        id: 1,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'DSC Lead',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        id: 4,
+        name: 'Aditya Srivasatava',
+        img: 'https://github.com/IronicDeGawd.png',
+        title: 'App Development',
+        twitter: 'https://twitter.com/0xironyAditya',
+        github: 'https://github.com/IronicDeGawd',
     },
     {
-        id: 2,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        id: 5,
+        name: 'Pranav Tripathi',
+        img: 'https://github.com/prnvtripathi.png',
+        title: 'Web Development',
+        twitter: 'https://twitter.com/prnvtwts',
+        github: 'https://github.com/prnvtripathi',
     },
     {
-        id: 3,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        id: 6,
+        name: 'Aahana Shukla',
+        img: 'https://github.com/Aahana-0204.png',
+        title: 'Outreach',
+        twitter: 'https://x.com/AahanaShukla2',
+        github: 'https://github.com/Aahana-0204',
     },
     {
-        id: 1,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'DSC Lead',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        id: 7,
+        name: 'Ishita Malik',
+        img: 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/ishita_malik_kJ8lz6J.jpg',
+        title: 'Design',
+        twitter: 'https://twitter.com/malikishita805',
+        github: 'https://github.com/ishita8839',
     },
     {
-        id: 2,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        id: 8,
+        name: 'Himanshu Chhatwal',
+        img: 'https://github.com/himanshu1221.png',
+        title: 'Outreach & Sponsorship',
+        twitter: 'https://twitter.com/himanshuchhatw2',
+        github: 'https://github.com/himanshu1221',
     },
     {
-        id: 3,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
-    },
-    {
-        id: 1,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'DSC Lead',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
-    },
-    {
-        id: 2,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
-    },
-    {
-        id: 3,
-        name: 'Keshav Malik',
-        img: 'https://picsum.photos/200',
-        title: 'CEO',
-        twitter: 'https://twitter.com',
-        instagram: 'https://instagram.com',
-        github: 'https://github.com',
+        id: 9,
+        name: 'Prateek Tiwari',
+        img: 'https://res.cloudinary.com/dgozpfjis/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1696741236/IMG_20231008_101633_rjdkjm.jpg?_s=public-apps',
+        title: 'Event Management',
+        twitter: 'https://x.com/t_prateek23',
+        github: 'https://github.com/tomioka-sensei',
     },
 ]
 
@@ -128,19 +92,16 @@ const TeamSection = () => {
                         return (
                             <div key={member.id} className="flex flex-col justify-center items-center gap-4 w-1/4 mb-8">
                                 <div>
-                                    <img className='rounded-full border-8 border-yellow-400' src={member.img} alt={member.name} width={175} />
+                                    <img className='rounded-full border-8 border-yellow-400 aspect-square' src={member.img} alt={member.name} width={175} />
                                 </div>
                                 <div>
                                     <h3 className='text-2xl font-semibold'>{member.name}</h3>
                                     <p className='text-blue-500 font-semibold'>{member.title}</p>
-                                    <div className="flex justify-center items-center gap-4 bg-blue-500 p-4 text-2xl text-white rounded-full mt-4">
-                                        <Link href={member.twitter} target="_blank" rel="noreferrer">
+                                    <div className="flex justify-center items-center gap-4 bg-blue-500 p-4 text-2xl text-white rounded-full mt-4 w-32 mx-auto">
+                                        <Link href={member.twitter} target="_blank" rel="noreferrer" className='transition hover:text-black'>
                                             <FaTwitter />
                                         </Link>
-                                        <Link href={member.instagram} target="_blank" rel="noreferrer">
-                                            <FaInstagram />
-                                        </Link>
-                                        <Link href={member.github} target="_blank" rel="noreferrer">
+                                        <Link href={member.github} target="_blank" rel="noreferrer" className='transition hover:text-black'>
                                             <FaGithub />
                                         </Link>
                                     </div>

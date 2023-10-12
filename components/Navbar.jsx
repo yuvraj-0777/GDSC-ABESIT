c66811bb9c3659055495ed1989e7250aa6d9b24c
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2023-10-12 22:01:13 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,26 +11,26 @@ const montserrat = Montserrat({
 });
 
 const Navbar = () => {
-   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-   const toggleMenu = () => {
-     setIsOpen(!isOpen);
-   };
-  
-    return (
-        <nav className={` md:flex md:items-center md:gap-8 text-base font-medium md:ml-4 hidden fixed bg-white dark:bg-black text-black dark:text-white ${montserrat.className}`}>
-           <div className="container mx-auto px-4 md:px-8">
-              <div className="flex items-center justify-between py-4">
-                <Link href="/">
-                    <div className="flex items-center">
-                        <Image src={Logo} alt="logo" width={60} />
-                        <h2>Google Developer Student Club</h2>
-                    </div>
-                    <p className="ml-14 -mt-3 text-xs">ABES Institute of Technology</p>
-                </Link>
-                
-           <div className="md:hidden">
-           
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className={` md:flex md:items-center md:gap-8 text-base font-medium md:ml-4 hidden fixed bg-white dark:bg-black text-black dark:text-white ${montserrat.className}`}>
+      <div className="container mx-auto px-4 md:px-8">
+        <div className="flex items-center justify-between py-4">
+          <Link href="/">
+            <div className="flex items-center">
+              <Image src={Logo} alt="logo" width={60} />
+              <h2>Google Developer Student Club</h2>
+            </div>
+            <p className="ml-14 -mt-3 text-xs">ABES Institute of Technology</p>
+          </Link>
+
+          <div className="md:hidden">
+
             <button
               className="mobile-menu-button"
               onClick={toggleMenu}
@@ -69,34 +69,30 @@ const Navbar = () => {
               )}
             </button>
           </div>
-            </div>
-          <ul
-            className={` md:flex md:items-center md:gap-8 text-base font-medium md:ml-4 hidden`}
-          >
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/">Event</Link></li>
-            <li><Link href="/">Project</Link></li>
-            <li><Link href="/team">Team</Link></li>
-            <li><Link href="/Contributors">Contributors</Link></li>
-          </ul>
-
-          
-          <ul
-              className={`${
-                isOpen ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"
-              } absolute top-full left-0 right-0 md:hidden transition-all duration-300 transform origin-top ease-in-out bg-white dark:bg-black bg-opacity-90`}
-            >
-            <li className=" border-y-2 pl-10 py-2"><Link href="/">Home</Link></li>
-            <li className=" border-b-2 pl-10 py-2"><Link href="/">Event</Link></li>
-            <li className=" border-b-2 pl-10 py-2"><Link href="/">Project</Link></li>
-            <li className=" border-b-2 pl-10 py-2"><Link href="/team">Team</Link></li>
-            <li className=" border-b-2 pl-10 py-2"><Link href="/Contributors">Contributors</Link></li>
-          </ul>
-            <ThemeButton />
-            </div>
-          </div>
-        </nav>
-    )
+        </div>
+        <ul
+          className={` md:flex md:items-center md:gap-8 text-base font-medium md:ml-4 hidden`}
+        >
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/">Event</Link></li>
+          <li><Link href="/">Project</Link></li>
+          <li><Link href="/team">Team</Link></li>
+          <li><Link href="/Contributors">Contributors</Link></li>
+        </ul>
+        <ul
+          className={`${isOpen ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"
+            } absolute top-full left-0 right-0 md:hidden transition-all duration-300 transform origin-top ease-in-out bg-white dark:bg-black bg-opacity-90`}
+        >
+          <li className=" border-y-2 pl-10 py-2"><Link href="/">Home</Link></li>
+          <li className=" border-b-2 pl-10 py-2"><Link href="/">Event</Link></li>
+          <li className=" border-b-2 pl-10 py-2"><Link href="/">Project</Link></li>
+          <li className=" border-b-2 pl-10 py-2"><Link href="/team">Team</Link></li>
+          <li className=" border-b-2 pl-10 py-2"><Link href="/Contributors">Contributors</Link></li>
+        </ul>
+        <ThemeButton />
+      </div>
+    </nav >
+  )
 }
 
 export default Navbar;

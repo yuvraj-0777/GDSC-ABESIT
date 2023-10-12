f8ef3355759040cc601cd70149fb0e7847fe92cf
Pranav Tripathi <tripathipranav14@gmail.com>
2023-10-12 22:18:05 +0530

    footer responsive

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ export default function Home() {
       <Navbar />
       <Hero />
       {/* Other components */}
-      <TeamSection />
       <PreviousEvents/>
       <Image src={image2} />
       <Contact />
